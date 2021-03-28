dfa2451f4b574af41f63db5125f840949ce2328e
rana-093 <masudrana201505093@gmail.com>
2021-03-28 16:06:47 +0600

    Something modified again

--- src/main/java/net/therap/model/BuildingProvider.java
+++ src/main/java/net/therap/model/BuildingProvider.java
@@ -5,9 +5,6 @@ package net.therap.model;
  * @since 28/03/2021
  */
 public class BuildingProvider implements SizeAnnotationValueProvider<Building> {
-    public BuildingProvider() {
-    }
-
     public int getSize(Building building) {
         return building.getFloorCount();
     }
